33ce664f70b7d0042ff5f49b43693d0fa28e729e
pedromml <pedrommleao@gmail.com>
2024-02-06 20:55:37 -0300

    Initial commit

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -1,11 +1,41 @@
+import { useRef } from "react";
 import "./ProfilePage.css";
+import songs from "../data/songs/songs.json";
 
 import user from "../data/users/user1.json";
 
 const ProfilePage = () => {
+  const horizontalScrollRef = useRef();
+  const onWheel = (e) => {
+    e.preventDefault();
+    try {const elm = horizontalScrollRef.current;
+    if (elm) {
+      if (e.deltaY == 0) return;
+      elm.scrollTo({
+        left: elm.scrollLeft + e.deltaY/2,
+      });
+      e.preventDefault();
+    }}
+    catch {
+      console.log("AAA")
+    }
+  };
+
   const age = Math.floor(
     (Date.now() - Date.parse(user.birthDate)) / (1000 * 60 * 60 * 24 * 365.25)
   );
+  const songList = songs.songs.map((song) => {
+    return (
+      <div className="song-picture-container" key={song.name}>
+        <img
+          className="song-picture"
+          src={require("../data/songs/album_covers/" + song.albumCover)}
+        />
+        <div className="song-picture-subtitle">{song.songName}</div>
+      </div>
+    );
+  });
+
   return (
     <div className="content">
       <div className="content-container">
@@ -15,7 +45,7 @@ const ProfilePage = () => {
         />
         <div className="text-container">
           <p className="profile-data-tag">Name:</p>
-          <p className="profile-text-data">{user.fullName}</p>
+          <input className="profile-text-data" value={user.fullName}/>
 
           <p className="profile-data-tag">Age:</p>
           <p className="profile-text-data">{age}</p>
@@ -25,24 +55,9 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="content-container content-container-flex-grow">
-        <div className="text-container">
+        <div className="picture-list-container">
           <p className="list-title">Liked songs</p>
-          <div className="picture-list-horizontal">
-            <div className="song-picture-container">
-              <img
-                className="song-picture"
-                src={require("../data/profile_photos/" + user.profile_picture)}
-              />
-              <div className="song-picture-subtitle">Nome da música</div>
-            </div>
-            <div className="song-picture-container">
-              <img
-                className="song-picture"
-                src={require("../data/profile_photos/" + user.profile_picture)}
-              />
-              <div className="song-picture-subtitle">Nome da música</div>
-            </div>
-          </div>
+          <div className="picture-list-horizontal" ref={horizontalScrollRef} onWheel={onWheel}>{songList}</div>
         </div>
       </div>
     </div>
